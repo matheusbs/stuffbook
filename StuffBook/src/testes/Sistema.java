a5506375aa0ddd217800f3b03c43f989d70e1019
matheus.ufcg@gmail.com <matheus.ufcg@gmail.com@8e27a49a-c310-4b69-dbb4-b2b09fa037bb>
2011-09-09 01:56:39 +0000

    commitei um exemplo de como fazer os testes, e o jar do easyAccept.

---
+++ StuffBook/src/testes/Sistema.java
@@ -0,0 +1,30 @@
+package testes;
+
+import java.util.ArrayList;
+
+public class Sistema {
+	
+	ArrayList<Usuario> usuarios;
+	
+	public Sistema(){
+		usuarios = new ArrayList<Usuario>();
+	}
+
+	public void criarUsuario(String login, String nome, String endereco) {
+		usuarios.add(new Usuario(login, nome, endereco));
+
+	}
+
+	public String getAtributoUsuario(String login, String atributo) {
+		if(atributo.equalsIgnoreCase("nome")){
+			for(Usuario u : usuarios){
+				if(u.login.equalsIgnoreCase(login)){
+					return u.nome;
+				}
+			}
+		}
+		
+		return "Usuario nao encontrado";
+	}
+
+}
---
+++ StuffBook/src/testes/Usuario.java
@@ -0,0 +1,14 @@
+package testes;
+
+
+public class Usuario {
+	
+	String login, nome, endereco;
+	
+	public Usuario(String login, String nome, String endereco){
+		this.login = login;
+		this.nome = nome;
+		this.endereco = endereco;
+	}
+
+}
