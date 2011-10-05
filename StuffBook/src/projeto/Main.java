fac3723d337591ad331f64e26035493cd3331a5c
pauloabsouto@gmail.com <pauloabsouto@gmail.com@8e27a49a-c310-4b69-dbb4-b2b09fa037bb>
2011-10-05 02:20:34 +0000

    comittando aqui pra ve se ajeita meu svn

--- StuffBook/src/projeto/Main.java
+++ StuffBook/src/projeto/Main.java
@@ -1,68 +1,31 @@
 package projeto;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Scanner;
-
 public class Main {
 
-	private static Sistema sistema;
 
 	public static void main(String args[]) throws Exception {
 
-		BufferedReader entradaString = new BufferedReader(
-				new InputStreamReader(System.in));
+		Sistema sis = new Sistema();
 
-		Scanner entradaNumeros = new Scanner(System.in);
+		System.out.println("testando\n");
 
-		sistema = new Sistema();
-		int opcao = 0, opcaoAuxiliar = 0;
-		String login, nome, endereco, idSessao = sistema.abrirSessaoDefault;
+		sis.criarUsuario("mark", "MARK DONO DO FB", "CASA DO CARAI");
+		String meuID = sis.abrirSessao("mark");
+		String idItem = sis.cadastrarItem(meuID, "piCA", "desc", "jogo");
+		sis.criarUsuario("batista", "mechupA", "NO PENIS");
+		String id2 = sis.abrirSessao("batista");
+		sis.requisitarAmizade(meuID, "batista");
+		sis.aprovarAmizade(id2, "mark");
+		String idRequisicaoEmprestimo = sis
+				.requisitarEmprestimo(id2, idItem, 7);
 
-		System.out.println("Bem Vindo ao StuffBook \n");
+		System.out.println("requerer: " + idRequisicaoEmprestimo);
+		System.out.println("aprova: "
+				+ sis.aprovarEmprestimo(meuID, idRequisicaoEmprestimo));
+		System.out.println("getEmprestimo: "
+				+ sis.getEmprestimos(meuID, "emprestador"));
 
-		do {
-			System.out.println("1 - Logar");
-			System.out.println("2 - Criar Usuario");
-			System.out.println("3 - Sair\n");
-			opcao = entradaNumeros.nextInt();
-
-			switch (opcao) {
-
-			case 1:
-
-				Usuario usuarioLogado;
-
-				System.out.println("Login: ");
-				login = entradaString.readLine();
-				usuarioLogado = sistema.procuraUsuarioLogin(login);
-				String sessao = sistema.abrirSessao(login);
-				System.out.println("1 - Cadastrar Item. ");
-				System.out.println("2 - Localizar Usuario. ");
-				System.out.println("3 - Adicionar Amigo. ");
-				System.out.println("4 - Excluir Amigo. ");
-				System.out.println("5 - Visualizar Perfil. ");
-				System.out.println("6 - Realizar Empréstimo. ");
-				System.out.println("7 - Devolver Item. ");
-				System.out.println("8 - Enviar Mensagem. ");
-				System.out.println("9 - Logoff. \n");
-				opcaoAuxiliar = entradaNumeros.nextInt();
-				break;
-
-			case 2:
-
-				System.out.println("Login: ");
-				login = entradaString.readLine();
-				System.out.println("Nome: ");
-				nome = entradaString.readLine();
-				System.out.println("Endereço: ");
-				endereco = entradaString.readLine();
-				sistema.criarUsuario(login, nome, endereco);
-				System.out.println("Usuario criado com sucesso. \n");
-				break;
-
-			}
-		} while (opcao != 3);
+		sis.devolverItem(id2, idRequisicaoEmprestimo);
 
 	}
 
